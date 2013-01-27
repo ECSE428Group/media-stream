1e8fd67f0fdcc78306ed39faaa2d7176ede111d6
Peter Henderson <peter.henderson@mail.mcgill.ca>
2013-01-27 09:15:03 -0500

    Update Video to Use Grids
    Updated the Video Player to use the displayed files rather than the
    test file uploader. Should work remotely if meteor is running.

--- media-site/client/main.js
+++ media-site/client/main.js
@@ -12,9 +12,8 @@ Template.audiogrid.contents = function () {
 Template.videogrid.events({
   'click': function (data) {
     $('#player-video').empty();
-    jwplayer("player-video").setup({
-      file : data.currentTarget.innerText
-    });
+    var file = data.currentTarget.innerText;
+    videoPlayerInit(file);
   }
 });
 
@@ -38,3 +37,55 @@ Meteor.call('getMedia', mediaPath, function (error, result) {
   Session.set("video-contents", result.video);
   Session.set("audio-contents",result.audio);
 });
+
+
+function videoPlayerInit(file) {
+      displayMessage();
+      playSelectedFile(file);
+       // inputNode = document.querySelector('input');
+    //  if (!URL) {
+    //      displayMessage('Your browser is not ' + '<a href="http://caniuse.com/bloburls">supported</a>!', true);
+    //      return;
+    // }
+      $('#select-modal').addClass("show");
+   // inputNode.addEventListener('change', playSelectedFile, false);
+      $(".video-modal .close").click(function(){
+        $(this).closest(".video-modal").removeClass("show");
+        $(this).next('video').get(0).pause();
+    });
+}
+
+function playSelectedFile(file) {
+            var videoNode = document.querySelector('video');
+            //var canPlay = videoNode.canPlayType(type);
+            //canPlay = (canPlay === '' ? 'no' : canPlay);
+            //var message = 'Can play type "' + type + '": ' + canPlay;
+            //var isError = canPlay === 'no';
+            //displayMessage(message, isError);
+            //if (isError) {
+            //    return;
+            //}
+          videoNode.src = file;
+          console.log(videoNode.src);
+}
+
+function displayMessage() {
+            var node = document.querySelector('#message');
+
+            return function displayMessage(message, isError) {
+                node.innerHTML = message;
+                node.className = isError ? 'error' : 'info';
+            };
+        }
+
+function isSupported(path) {
+  var supportedFiletypes = [ ".mp4" ];
+  for (var i = 0; i < supportedFiletypes.length; i++) {
+    if (path.indexOf(supportedFiletypes[i]) != -1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+

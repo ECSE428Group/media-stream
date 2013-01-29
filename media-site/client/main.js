730455dbd9a2ee150719a138a5d6ae3e7c546f7a
Rathesh Saba <rathesh.saba@gmail.com>
2013-01-29 13:44:11 -0500

    Cleaned up folders and added audio.js to lib

--- media-site/client/main.js
+++ media-site/client/main.js
@@ -11,20 +11,60 @@ Template.audiogrid.contents = function () {
 
 Template.videogrid.events({
   'click': function (data) {
-    $('#player-video').empty();
+    /*$('#player-video').empty();
     jwplayer("player-video").setup({
       file : data.currentTarget.innerText
-    });
+    });*/
   }
 });
 
 
 Template.audiogrid.events({
   'click': function (data) {
+    var selected = data.currentTarget.innerText;
     $('#player-audio').empty();
-    jwplayer("player-audio").setup({
+    $('#player-audio').jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia",{
+          mp3 : selected
+        }).jPlayer("play");
+      },
+	swfPath: '../js',
+ 	solution: 'html, flash',
+  	supplied: 'mp3',
+   //	preload: 'metadata',
+   height:'100px',
+   width:'100px',
+    volume: 0.8,
+    muted: false,
+    backgroundColor: '#000000',
+    cssSelectorAncestor: '#player-audio',
+    cssSelector: {
+        videoPlay: '.jp-video-play',
+    	play: '.jp-play',
+        pause: '.jp-pause',
+        stop: '.jp-stop',
+        seekBar: '.jp-seek-bar',
+        playBar: '.jp-play-bar',
+        mute: '.jp-mute',
+        unmute: '.jp-unmute',
+        volumeBar: '.jp-volume-bar',
+        volumeBarValue: '.jp-volume-bar-value',
+        volumeMax: '.jp-volume-max',
+        currentTime: '.jp-current-time',
+        duration: '.jp-duration',
+        fullScreen: '.jp-full-screen',
+        restoreScreen: '.jp-restore-screen',
+        repeat: '.jp-repeat',
+        repeatOff: '.jp-repeat-off',
+        gui: '.jp-gui',
+       noSolution: '.jp-no-solution'
+    },
+    errorAlerts: false,
+    warningAlerts: false});
+  /*  jwplayer("player-audio").setup({
       file : data.currentTarget.innerText
-    });
+    });*/
   }
 });
 

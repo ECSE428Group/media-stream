76cbb161adb9469ebef17d8a71dde9480b4ab920
Lecernawx <smith@lecernawx.com>
2013-03-15 17:48:34 -0400

    More fixes
    Because

--- media-site/client/login.js
+++ media-site/client/login.js
@@ -2,6 +2,24 @@
 // All logon procedures and template events are stored here.
 
 // Template Definition ----------------------------------------------
+Template.profile-page.events(
+{
+	'click #saveChanges': function()
+	{
+		var old_pass = $('#profile-password').val();
+		var new_pass = $('#profile-newpassword').val();
+		var cnf_pass = $('#profile-confirmpass').val();
+
+		if (new_pass != cnf_pass)
+			show_error("Bad confirmation.");
+
+		if (old_pass == "" || new_pass == "" || cnf_pass == "")
+			show_error("No empty fields.");
+
+		Accounts.changePassword(old_pass, new_pass);
+	},
+};
+
 Template.loginpage.events({
 
 	// LOGIN ====================================================

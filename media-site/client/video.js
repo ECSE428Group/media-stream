b85229c7b8a70b102ca33a7500fee2776f46e658
Alison <alison@ubuntu.(none)>
2013-03-25 01:07:44 -0400

    upload fixes

--- media-site/client/video.js
+++ media-site/client/video.js
@@ -514,11 +514,12 @@ Meteor.saveFile = function(blob, name, path, type, callback) {
       encoding = 'binary';
       break;
   }
-
   fileReader.onload = function(file) {
     Meteor.call('saveFile', file.srcElement.result, name, path, encoding, callback);
   };
+
   fileReader[method](blob);
+
 };
 
 function onLoad() {
